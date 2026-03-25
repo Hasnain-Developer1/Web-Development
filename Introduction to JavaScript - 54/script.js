d584fbd545c5a4e01db29ebdb1c2dc0fa131d6f7
hasnain-developer1 <hasnainmalik9925@gmail.com>
2026-03-25 17:31:01 +0500

    Now I started learning JavaScript and Here I installed node js

---
+++ Introduction to JavaScript - 54/script.js
@@ -0,0 +1,23 @@
+alert("Hello World")
+
+console.log("Code is running...")
+console.log("Code is also running...")
+console.log("Code is looking like a wow...")
+
+var a = prompt("Enter your Number: ")
+var isTrue = confirm("Are you sure to leave this page and burn your computer?")
+
+if(isTrue){
+    console.log("Computer is blasting")
+}
+else{
+    console.log("Computer is not blasting")
+    
+
+}
+
+console.log("Your Number is " + a)
+
+document.title = "Hey I am good" //title of the website//
+
+document.body.style.backgroundColor = "red"
