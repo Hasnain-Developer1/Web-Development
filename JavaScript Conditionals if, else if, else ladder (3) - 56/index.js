a4651f443a94cac8bda57dbfdbe0a42eee60e335
hasnain-developer1 <hasnainmalik9925@gmail.com>
2026-03-26 15:53:06 +0500

    Here I learned JavaScript Conditionals: if, else if, else ladder

---
+++ JavaScript Conditionals if, else if, else ladder (3) - 56/index.js
@@ -0,0 +1,58 @@
+console.log("Hello I am conditional tutorial ")
+
+let age = 1
+// let grace = 2
+// age += grace
+
+// console.log(age)
+// console.log(age + grace)
+// console.log(age - grace)
+// console.log(age / grace)
+// console.log(age % grace)
+// console.log(age ** grace)
+
+/*
+I am 
+a multiline 
+comment 
+*/
+
+if(age == 18){
+    console.log("You can drive")
+}
+else if(age == 0){
+    console.log("Are you kiddng")
+}
+else if(age == 1){
+    console.log("Are you again kiddng")
+}
+else{
+console.log("You cannot drive")
+}
+// if("3" == 3) answer is true as == will check only the values not data types
+// if("3" === 3) answer is not true as === will also check the data type
+
+/*
+if("3" === 3){
+    console.log("Equal")
+}
+else {
+    console.log("Not Equal")
+    console.log("Not Equal")
+
+}
+    
+*/
+let a = 5
+let b = 6
+let c = a>b?(a+b):(a-b)
+console.log(c)
+/*
+if(a>b){
+let c = a+b
+}
+else{
+    let c = a-b
+    }
+*/
+
