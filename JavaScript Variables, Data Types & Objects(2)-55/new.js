aa6187da63a7dcd383d7ae78c2cf147768406438
hasnain-developer1 <hasnainmalik9925@gmail.com>
2026-03-26 11:36:40 +0500

    Here I learned JS Variables, Data Types & Object

---
+++ JavaScript Variables, Data Types & Objects(2)-55/new.js
@@ -0,0 +1,47 @@
+console.log("Hey This is the tutorial 55 of JS")
+
+var a = 56;
+let b = 6;
+let c = "Hasnain";
+let _a = "Malik";
+//     var 55a = "Malik" ----->  Not allowed 
+// console.log(a + b + 8)
+
+console.log(a);
+
+const d1 = 6;
+// const ke value change ni kr skta 
+// d1 = d1 + 1   // const = const = 1 bhe ni kr skta
+
+// console.log(typeof a, typeof b, typeof c)
+
+{
+       let a = 50; // only in block
+    // var a = 50; // globally scope
+    console.log(a);
+    
+}
+console.log(a);
+
+
+let x = "Hasnain Malik";
+let y = 32;
+let z = 3.62;
+const p = true;
+let q = undefined;
+let r = null;
+
+
+console.log(x, y, z, p, q, r);
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r);
+
+let o = {
+    name: "Hasnain MAlik",
+    "Job Code": 5600,
+    is_handsome: true,
+}
+console.log(o);
+o.salary = "100K";
+console.log(o);
+o.salary = "500K"
+console.log(o);
