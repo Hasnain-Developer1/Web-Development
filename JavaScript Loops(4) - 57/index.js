ba923825b5260b75af712195ea4fa2ca5444bcc4
hasnain-developer1 <hasnainmalik9925@gmail.com>
2026-03-26 16:44:56 +0500

    Here I learned JavaScript Loops

---
+++ JavaScript Loops(4) - 57/index.js
@@ -0,0 +1,43 @@
+console.log("Hey I am a tutorial on Loop")
+
+let a = 1
+// console.log(a)
+// console.log(a+1)
+// console.log(a+2)
+// console.log(a+3)
+
+// for (let i = 0; i< 100; i++) {
+//     console.log(a+i)
+    
+// }
+
+
+// let obj = {
+//     name: "Hasnain Malik",
+//     Role: "Programmer",
+//     Age: 19
+// }
+// for (const key in obj) {
+//     const element = obj[key];
+//     console.log(key, element)
+    
+    
+// }
+
+
+// for (const q of   "Hasnain") {
+//     console.log(q)
+// }
+
+
+// let i = 1;
+// while (i<100) {
+//     console.log(i)
+//     i++
+// }
+
+let i =0
+do {
+    console.log(i)
+    i++
+} while (i<6);
