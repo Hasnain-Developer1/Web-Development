1bea6154a62114cc57eb3805bd21766d2ab3ef30
hasnain-developer1 <hasnainmalik9925@gmail.com>
2026-03-26 22:42:44 +0500

    Here I learned JavaScript Strings

---
+++ JavaScript Strings (7) - 60/index.js
@@ -0,0 +1,49 @@
+console.log("This is String tutorial ")
+let a = "Hasnain Malik"
+console.log(a)
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[30])
+
+
+console.log(a.length)
+
+let real_name = "Hasnain"
+let friend = "Umair"
+
+
+console.log("His name is " + real_name + " And his friend name is " + friend)
+
+console.log(`His Name is ${real_name} and his friend name is ${friend}`)
+
+
+let b = "Umair Mushtaq"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+// console.log(b.tolowerCase())
+// console.log(b.toUpperCase())
+
+
+
+console.log(b.slice(1, 5))
+
+console.log(b.replace("Has", "77"))
+
+console.log(b.concat(a, " Malik ", "is ", "another name"))
+
+
+//name.trim()  // remove white spaces
+
+// String is immutable ap string ko change ni kr skta
+
+console.log(b.endsWith("taq"))
+console.log(b.startsWith("Umair"))
+
+
+
+
+
+
+
