5c99810c299a6370953c9dfa4e4ef65690c03694
hasnain-developer1 <hasnainmalik9925@gmail.com>
2026-03-27 17:30:39 +0500

    Here I learned JavaScript Arrays

---
+++ JavaScript Arrays - 63/index.js
@@ -0,0 +1,44 @@
+let arr = [1,8,3,2,6,0,4]
+// index   0,1,2,3,4,5,6
+arr[0] = 566
+
+// console.log(arr, typeof arr)
+
+
+// console.log(arr.length)
+// console.log(arr[0])
+// console.log(arr[2])
+// console.log(arr[6])
+// console.log(arr.toString())
+// console.log(arr.join(" and "))
+
+let a = [1,2,3,4,5,6,7,8,9,0]
+// a.pop()
+// console.log(a)
+// a.push(100)
+// console.log(a)
+// a.push("Hasnain")
+
+// a.shift()
+// console.log(a)
+// a.unshift(100)
+// console.log(a)
+
+// delete a[9]
+// console.log(a)
+// console.log(a.length)
+
+let a1 = [1,2,3,4,5,6,7,8,9]
+
+// console.log(a1.concat(a2, a3))
+// console.log(a2)
+// console.log(a1.sort())
+// a1.splice(0,5,2,0)
+// console.log(a1)
+a1.slice(0,5)
+console.log(a1)
+
+
+
+
+
