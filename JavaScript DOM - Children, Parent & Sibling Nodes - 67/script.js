47599ba9f8958dd7b174c421b1f6784d1ade0ace
hasnain-developer1 <hasnainmalik9925@gmail.com>
2026-03-28 15:55:25 +0500

    Here I learned JavaScript DOM - Children, Parent & Sibling Nodes

---
+++ JavaScript DOM - Children, Parent & Sibling Nodes - 67/script.js
@@ -0,0 +1,9 @@
+console.log("Hello world ")
+
+// Hello world 
+document.body.firstElementChild
+{/* <div class=​"conatiner">​…​</div>​  */}
+document.body.firstElementChild.childNodes
+NodeList(11) [text, div.box, text, div.box, text, div.box, text, div.box, text, div.box, text]
+document.body.firstElementChild.children
+HTMLCollection(5) [div.box, div.box, div.box, div.box, div.box]
