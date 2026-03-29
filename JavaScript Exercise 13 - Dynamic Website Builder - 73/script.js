3f2a6304b6bdf752239ede75b1dd4d0704bbf2aa
hasnain-developer1 <hasnainmalik9925@gmail.com>
2026-03-29 10:34:43 +0500

    JavaScript Exercise 13 - Dynamic Website Builder

---
+++ JavaScript Exercise 13 - Dynamic Website Builder - 73/script.js
@@ -0,0 +1,5 @@
+function createcard(title, cname, views, monthsold, duration, thumbnail){
+    // finsih this function
+}
+
+createcard("Introduction to BackEnd | Sigma Web Dev Video #2", "Hasnian Malik", 560000, 7, "31:22", "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEnCPYBEIoBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLCKktmHuXV_macV6MoUwhKxwzXJ7w" )
