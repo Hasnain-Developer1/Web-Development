44bc1c4d0e38339972920827c9428dae6ee83d71
hasnain-developer1 <hasnainmalik9925@gmail.com>
2026-04-19 00:36:20 +0500

    JAVA Script File

---
+++ CommonJs Vs EcmaScript Modules-86/main.js
@@ -0,0 +1,14 @@
+// import {a,b,d} from "./mymodule.js"
+// console.log(a,b,d);
+
+
+// import Hasnian from "./mymodule.js"
+// console.log(Hasnian);
+
+// (function(exports, require, module, __dirname, __filename){
+
+//     // Module Code actually lives here
+
+// })
+const a = require("./mymodule2.js")
+console.log(a, __dirname, __filename);
