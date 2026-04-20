885e93f03a43b47070e8845a25f2603ef39ddaeb
hasnain-developer1 <hasnainmalik9925@gmail.com>
2026-04-20 23:11:40 +0500

    Exercise 15 - Clear the Clutter

---
+++ Exercise 15 - Clear the Clutter - 91/index.js
@@ -0,0 +1,21 @@
+/* You have to write a Node.js program to clear the clutter inside of a directory and organize the content of the directory into different folder 
+
+For Example these file become:
+
+  1. name.jpg
+  2. name.png
+  3. this.pdf
+  4. Hasnain.zip
+  5. Ayyan.zip
+  6. Cat.jpg   
+  7. Hasnain.pdf
+
+  this:
+  jpg/name.jpg, jpg/Cat.jpg
+  png/name.png
+  pdf/this.pdf, pdf/Hasnain.pdf
+  zip/Hasnain.zip, zip/Ayyan.zip
+  
+  
+  
+  */
