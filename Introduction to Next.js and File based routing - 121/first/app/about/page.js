bd3008d0cbeccd005158cf85152ada6d770f2054
hasnain-developer1 <hasnainmalik9925@gmail.com>
2026-05-24 22:33:11 +0500

    page.js

---
+++ Introduction to Next.js and File based routing - 121/first/app/about/page.js
@@ -0,0 +1,18 @@
+
+import React from 'react'
+
+const Contact = () => {
+    return (
+        <div>
+            <h1 className="text">Problem solved by Next.js</h1>
+            <ul>
+                <li>File based routing</li>
+                <li>File rendering</li>
+                <li>Additional features like next routing from next/navigation</li>
+                <li>Optimized Rendering</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Contact
