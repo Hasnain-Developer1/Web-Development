a6367dfb78e14f84585f97c25ff7356a3bc58417
hasnain-developer1 <hasnainmalik9925@gmail.com>
2026-05-26 07:48:02 +0500

    Here I learned the Server and client components in Next.js

---
+++ Server Components in Next.js - 122/my-app/app/page.js
@@ -0,0 +1,20 @@
+// "use client"
+// import { useState, useEffect } from "react";
+import fs from "fs/promises";
+import Navbar from "../public/components/Navbar";
+
+export default function Home() {
+  // const [count, setCount] = useState(0)
+  console.log("Hey I am Hasnain Malik")
+  let a =  fs.readFile(".gitignore")
+  a.then(e=>{console.log(e.toString())})
+
+  return (
+    <div>
+      <Navbar/>
+      I am a component 
+      {/* {count} */}
+      {/* <button onClick={() => setCount(count + 1)}>click me</button> */}
+    </div>
+  );
+}
