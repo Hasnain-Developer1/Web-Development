5bb6b57b9ab3d9d91d3f77144e4005f0552fbf31
hasnain-developer1 <hasnainmalik9925@gmail.com>
2026-06-20 18:29:53 +0500

    layout

---
+++ Script, Link & Image components in Next.js - 123/website/app/layout.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/footer";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Facbook - connect with the world",
+  description: "This is facebook and we can connect with the world using facebook",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">
+        <Navbar />
+        <div className="container mx-auto min-h-[80vh]">
+        {children}
+        </div>
+        <Footer />
+      </body>
+    </html>
+  );
+}
