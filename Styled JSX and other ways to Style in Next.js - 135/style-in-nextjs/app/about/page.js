388035b87bdd699d8df0e38c85619c1ccbd7fc0a
hasnain-developer1 <hasnainmalik9925@gmail.com>
2026-07-04 16:25:25 +0500

    About Page

---
+++ Styled JSX and other ways to Style in Next.js - 135/style-in-nextjs/app/about/page.js
@@ -0,0 +1,27 @@
+"use client";
+import React from "react";
+
+const About = () => {
+    return (
+        <>
+            <div>
+                <div className="content-box">
+                    <h1>This is About Me</h1>
+                    <p>Hey I am a Good Boy</p>
+                </div>
+
+                <div>Hey I am Good</div>
+            </div>
+
+            {/* {/* <style jsx global>{`  It will apply on the whole Page*/}
+            <style jsx>{`
+        .content-box {
+          background-color: red;
+          color: green;
+        }
+      `}</style>
+        </>
+    );
+};
+
+export default About;
